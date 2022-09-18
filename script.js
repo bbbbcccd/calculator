a10096bed3136d91a12b6e3b281a73f39b95f1a2
Zac <zacheryngky@gmail.com>
2022-09-18 18:37:18 +0800

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    switch (true) {
+        case (operator === '+'):
+            return add(num1, num2); 
+        
+        case (operator === '-'):
+            return subtract(num1, num2); 
+        
+        case (operator === '*'):
+            return multiply(num1, num2); 
+
+        case (operator === '/'):
+            return divide(num1, num2);   
+    
+        default: 
+            console.log("Error");
+            return false;
+    }
+}
