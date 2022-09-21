09fb7cfe6ea374290e10a6b363e8ddb6261e290b
Zac <zacheryngky@gmail.com>
2022-09-21 19:01:05 +0800

    Add event listener for number and dot buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const MAX_VALUE = 999999999999;
-const MIN_VALUE = -999999999999;
+const MAX_LENGTH = 12;
 const DEFAULT_VALUE = 0;
 let current_value = DEFAULT_VALUE;
 let display_value = DEFAULT_VALUE;
+const display_container = document.getElementById("result");
 const number_buttons = document.querySelectorAll(".number");
 const operator_buttons = document.querySelectorAll(".operator");
 const dot_button = document.getElementById("dot");
@@ -46,3 +46,25 @@ function operate(operator, num1, num2) {
             return false;
     }
 }
+
+// When number button is clicked, evaluate the new display string
+// Check if it is longer than the max length
+// Update the display_value variable 
+
+number_buttons.forEach(numNode => numNode.addEventListener("click", (e) => {
+    display_text = display_container.textContent + e.target.textContent;
+    if (display_text.length <= MAX_LENGTH) {
+        display_value = +display_text;
+        display_container.textContent = display_text;
+    }
+}));
+
+// When dot button is clicked, check if the display contains a dot
+// Do not do anything if the display already contains a dot
+// Else, append a dot to the end of the display
+
+dot_button.addEventListener("click", (e) => {
+    if (!display_container.textContent.includes(".")) {
+        display_container.textContent += ".";
+    }
+});
