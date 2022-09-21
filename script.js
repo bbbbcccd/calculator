e3a3b7976df627c76abd71f579ff70f79da9bc99
Zac <zacheryngky@gmail.com>
2022-09-21 18:18:27 +0800

    Add global variables
    Set global variables for:
    Min, Max and Default values
    stored and display values
    all buttons
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const MAX_VALUE = 999999999999;
+const MIN_VALUE = -999999999999;
+const DEFAULT_VALUE = 0;
+let current_value = DEFAULT_VALUE;
+let display_value = DEFAULT_VALUE;
+const number_buttons = document.querySelectorAll(".number");
+const operator_buttons = document.querySelectorAll(".operator");
+const dot_button = document.getElementById("dot");
+const all_clear_button = document.getElementById("AC");
+const clear_entry_button = document.getElementById("CE");
+const equals_button = document.getElementById("equals");
+const plus_minus_button = document.getElementById("plus-minus");
+
 function add(num1, num2) {
     return num1 + num2;
 }
